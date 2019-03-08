795b7292bb74d4c02b350c90e86b161fdb1623a1
Amon <guinan20@gmail.com>
2019-03-08 15:20:21 -0600

    airport V1

--- src/AUTOCLIENT.java
+++ src/AUTOCLIENT.java
@@ -1,6 +1,13 @@
 public class AUTOCLIENT {
     public static void main(String [] args)
     {
+AirportClass arprtclss = new AirportClass();
+arprtclss.Airport();
+arprtclss.Airport("DFW",8);
+System.out.println(arprtclss.getAirportCode());
+System.out.println(arprtclss.getGates());
+
+
 
 
     }
