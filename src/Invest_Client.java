541f56d9b974bd287bdf841d857af25b3d6aed99
Amon <guinan20@gmail.com>
2019-03-20 21:03:43 -0500

    Completed Client

--- src/Invest_Client.java
+++ src/Invest_Client.java
@@ -6,7 +6,7 @@ public class Invest_Client {
         investment.INPUT(100, 0.1, 2);
          double gain;
          gain = Math.pow(investment.getInvestment() * (1 + investment.getRate()), investment.getYears());
-         System.out.println("User invests "+investment.getInvestment()+" dollars for "+investment.getYears()+" at a rate of "+investment.getRate()+".");
+         System.out.println("User invests "+investment.getInvestment()+" dollars for "+investment.getYears()+" years at a rate of "+investment.getRate()+".");
          System.out.println("User profits "+gain+" dollars.");
     }
 }
