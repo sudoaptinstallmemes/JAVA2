6814cfdef1e2efc4f5fede188caf1a7d88be3c8b
Amon <guinan20@gmail.com>
2019-04-04 22:41:59 -0500

    Bubble Sort + Added lines

--- Chapter 8/src/Array.java
+++ Chapter 8/src/Array.java
@@ -1,3 +1,10 @@
+
+/*
+Amon Guinan
+Sorting Methods Program
+April 2019
+ */
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -10,14 +17,15 @@ public class Array {
         Array sort = new Array();
 
         sort.listGen();
-        sort.insertionSort();
-
+//        sort.insertionSort(); //Close to working, needs some fixing.
+        sort.bubbleSort(); //Only one that works 100% right now.
+//        sort.mergeSort();  //Early stages
     }
 
     public void listGen() {
         Random random = new Random();
         int x = 0;
-        while (x != 11) {
+        while (x != 10) {
             TBS.add(random.nextInt(10));
             x++;
         }
@@ -36,7 +44,35 @@ public class Array {
         return listWrite();
     }
 
-    public void insertionSort () {
+    public ArrayList<Integer> insertionSort () {
+        ArrayList<Integer> correct = new ArrayList<>();
+        int temp = 10;
+        int i;
+        for(int x = 0; x<10; x++) {
+            for (i = 0; i < TBS.size()-1; i++) {
+
+                if (temp > TBS.get(i)) {
+                    temp = TBS.get(i);
+//                    System.out.println(i+","+temp);
+                }
+
+            }
+            TBS.remove(TBS.get(i-1));
+            System.out.println();
+            System.out.println("correct: "+correct);
+            System.out.println("TBS: "+TBS);
+            System.out.println();
+            correct.add(temp);
+            temp = 10;
+        }
+        TBS = correct;
+        System.out.println();
+        System.out.println(TBS);
+        System.out.println();
+        return(TBS);
+    }
+
+    public ArrayList<Integer> bubbleSort () {
         int x=0;
         int temp;
         for (int i = 0; i < 10; i++) {
@@ -54,6 +90,25 @@ public class Array {
         }
         System.out.println("Done!");
         System.out.println(TBS);
+        return(TBS);
+    }
+
+    public ArrayList<Integer> mergeSort () {
+        int x = 0;
+        int temp;
+        for (int i = 0; i < 5; i++)
+        {
+            if(TBS.get(x) > TBS.get(x+1))
+            {
+                temp = TBS.get(x + 1);
+                TBS.set(x + 1, TBS.get(x));
+                TBS.set(x, temp);
+                System.out.println();
+                System.out.println(TBS);
+            }
+            x = x+2;
+        }
+        return(TBS);
     }
 }
 
